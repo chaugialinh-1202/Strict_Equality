34ed3b35b46df498cfce4459fc748350f3e2e32e
turing-Linh <chaugialinh1202@gmail.com>
2026-05-20 12:50:16 +0700

    submit

--- main.js
+++ main.js
@@ -3,4 +3,5 @@ let stringB = "5"
 
 console.log("kết quả của == "  + (numberA == stringB));
 console.log("kết quả của === "  + (numberA === stringB));
-confirm("Bạn đã hiểu bài chưa?")
+console.log(confirm("Bạn đã hiểu bài chưa?"));
+
